4732603d4cbcbbb98238ae335078ceb5cf260989
Ivan <hubbu09@gmail.com>
2020-08-27 16:09:54 -0400

    Finish groundwork tests of ch1; Swap same for equals

--- src/tuple.lua
+++ src/tuple.lua
@@ -3,12 +3,28 @@ Tuple = {
   y = 0,
   z = 0,
   w = 0,
+
+  -- https://mathinsight.org/vectors_cartesian_coordinates_2d_3d
+  magnitude = function (self)
+    return math.sqrt(self.x^2 + self.y^2 + self.z^2 + self.w^2)
+  end,
+
+  normalize = function (self)
+    local mag = self:magnitude()
+    local a = Tuple:new(self.x / mag, self.y / mag, self.z / mag, self.w / mag)
+    return a
+  end,
+
+  dot = function (self, b)
+    -- The smaller the dot product, the larger the angle between the vectors
+    return self.x * b.x +
+           self.y * b.y +
+           self.z * b.z +
+           self.w * b.w
+  end,
   
   __add = function (a, b)
-    return Tuple:new(
-      a.x + b.x, a.y + b.y, 
-      a.z + b.z, a.w + b.w
-    )
+    return Tuple:new(a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w)
   end,
 
   __sub = function (a, b)
@@ -26,9 +42,29 @@ Tuple = {
     end
   end,
 
+  __unm = function (a)
+    return Tuple:new(0, 0, 0, 0) - a
+  end,
+
+  __mul = function (a, b)
+    return Tuple:new(a.x * b, a.y * b, a.z * b, a.w * b)
+  end,
+
+  __div = function (a, b)
+    return Tuple:new(a.x / b, a.y / b, a.z / b, a.w / b)
+  end,
+
+  __eq = function (a, b)
+    local epsilon = 0.00001
+
+    return math.abs(a.x - b.x) < epsilon
+      and math.abs(a.y - b.y) < epsilon
+      and math.abs(a.z - b.z) < epsilon
+      and math.abs(a.w - b.w) < epsilon
+  end,
+
   __tostring = function (self)
-    return "{" .. self.x .. ", " .. self.y .. ", " 
-      .. self.z .. ", " .. self.w  .."}"
+    return "{" .. self.x .. ", " .. self.y .. ", " .. self.z .. ", " .. self.w  .."}"
   end
 }
 
@@ -64,14 +100,9 @@ function Point:new(x, y, z)
     w = 1
   }
 
-  -- Todo: Create Point MT(?)
   setmetatable(t, Tuple)
   self.__index = self
   return t
-end
-
-function Point:type()
-    return "point"
 end
 
 Vector = {}
@@ -80,15 +111,19 @@ function Vector:new(x, y, z)
     x = x or 0,
     y = y or 0,
     z = z or 0,
-    w = 0
+    w = 0,
+
+    cross = function (self, b)
+      -- Returns a new vector that is perpendicular to both input vectors
+      return Vector:new(
+        self.y * b.z - self.z * b.y,
+        self.z * b.x - self.x * b.z,
+        self.x * b.y - self.y * b.x
+      )
+    end
   }
 
-  -- Todo: Create Vector MT (?)
   setmetatable(t, Tuple)
   self.__index = self
   return t
-end
-
-function Vector:type()
-    return "vector"
 end
--- unit/tuples_spec.lua
+++ unit/tuples_spec.lua
@@ -27,10 +27,8 @@ describe("Point:new() creates tuples with w=1", function ()
   local p = Point:new(4, -4, 3)
   local t = Tuple:new(4, -4, 3, 1)
 
-  assert.are.equal(p.x, t.x)
-  assert.are.equal(p.y, t.y)
-  assert.are.equal(p.z, t.z)
-  assert.are.equal(p.w, t.w)
+  assert.are.equals(p, t)
+  assert.are.equal(p.w, 1)
   assert.are.equal(p:type(), "point")
 end)
 
@@ -38,10 +36,8 @@ describe("Vector:new() creates tuples with w=0", function ()
   local v = Vector:new(4, -4, 3)
   local t = Tuple:new(4, -4, 3, 0)
 
-  assert.are.equal(v.x, t.x)
-  assert.are.equal(v.y, t.y)
-  assert.are.equal(v.z, t.z)
-  assert.are.equal(v.w, t.w)
+  assert.are.equals(v.x, t.x)
+  assert.are.equal(v.w, 0)
   assert.are.equal(v:type(), "vector")
 end)
 
@@ -49,29 +45,119 @@ describe("Adding two tuples", function ()
   local t1 = Tuple:new(3, -2, 5, 1)
   local t2 = Tuple:new(-2, 3, 1, 0)
 
-  assert.are.same(t1 + t2, Tuple:new(1, 1, 6, 1))
+  assert.are.equals(t1 + t2, Tuple:new(1, 1, 6, 1))
 
-  assert.are.same(Point:new(1,2,3) + Point:new(4,5,6), Tuple:new(5,7,9,2))
-  assert.are.same(Vector:new(1,2,3) + Vector:new(4,5,6), Tuple:new(5,7,9,0))
+  assert.are.equals(Tuple:new(5,7,9,2), Point:new(1,2,3) + Point:new(4,5,6))
+  assert.are.equals(Tuple:new(5,7,9,0), Vector:new(1,2,3) + Vector:new(4,5,6))
 end)
 
 describe("Subtracting two points", function ()
   local p1 = Point:new(3, 2, 1)
   local p2 = Point:new(5, 6, 7)
 
-  assert.are.same(p1 - p2, Vector:new(-2, -4, -6))
+  assert.are.equals(Vector:new(-2, -4, -6), p1 - p2)
 end)
 
 describe("Subtracting a vector from a point", function ()
   local p = Point:new(3, 2, 1)
   local v = Vector:new(5, 6, 7)
 
-  assert.are.same(p - v, Point:new(-2, -4, -6))
+  assert.are.equals(Point:new(-2, -4, -6), p - v)
 end)
 
 describe("Subtracting two vectors", function ()
-    local v1 = Vector:new(3, 2, 1)
-    local v2 = Vector:new(5, 6, 7)
+  local v1 = Vector:new(3, 2, 1)
+  local v2 = Vector:new(5, 6, 7)
 
-    assert.are.same(v1 - v2, Vector:new(-2, -4, -6))
+  assert.are.equals(Vector:new(-2, -4, -6), v1 - v2)
+end)
+
+describe("Subtracting a vector from the zero vector", function ()
+  local zero = Vector:new(0, 0, 0)
+  local v = Vector:new(1, -2, 3)
+
+  assert.are.equals(Vector:new(-1, 2, -3), zero - v)
+end)
+
+describe("Negating a tuple", function ()
+  local t = Tuple:new(1, -2, 3, -4)
+  
+  assert.are.equals(Tuple:new(-1, 2, -3, 4), -t)
+end)
+
+describe("Multiplying a tuple by a scalar", function ()
+  local t = Tuple:new(1, -2, 3, -4)
+
+  assert.are.equals(Tuple:new(3.5, -7, 10.5, -14), t * 3.5)
+end)
+
+describe("Multiplying a tuple by a fraction", function ()
+  local t = Tuple:new(1, -2, 3, -4)
+
+  assert.are.equals(Tuple:new(0.5, -1, 1.5, -2), t * 0.5)
+end)
+
+describe("Dividing a tuple by a scalar", function ()
+  local t = Tuple:new(1, -2, 3, -4)
+
+  assert.are.equals(Tuple:new(0.5, -1, 1.5, -2), t / 2)
+end)
+
+describe("Computing the magnitude of vector(1, 0, 0)", function ()
+  local v = Vector:new(1, 0, 0)
+
+  assert.are.equals(1, v:magnitude())
+end)
+
+describe("Computing the magnitude of vector(0, 1, 0)", function ()
+  local v = Vector:new(0, 1, 0)
+
+  assert.are.equals(1, v:magnitude())
+end)
+
+describe("Computing the magnitude of vector(1, 2, 3)", function ()
+  local v = Vector:new(1, 2, 3)
+
+  assert.are.equals(math.sqrt(14), v:magnitude())
+end)
+
+describe("Computing the magnitude of vector(-1, -2, -3)", function ()
+  local v = Vector:new(-1, -2, -3)
+
+  assert.are.equals(math.sqrt(14), v:magnitude())
+end)
+
+describe("Normalizing vector(4, 0, 0) gives (1, 0, 0)", function ()
+  local v = Vector:new(4, 0, 0)
+
+  assert.are.equals(Vector:new(1, 0, 0), v:normalize())
+end)
+
+describe("Normalizing vector(1, 2, 3)", function ()
+  local v = Vector:new(1, 2, 3)
+
+  --                   1/sqrt(14), 2/sqrt(14), 3/sqrt(14)
+  assert.are.equals(Vector:new(0.26726, 0.53452, 0.80178), v:normalize())
+end)
+
+describe("The magnitude of a normalized vector", function ()
+  local v = Vector:new(1, 2, 3)
+  local norm = v:normalize()
+
+  assert.are.equals(1, norm:magnitude())
+end)
+
+describe("The dot product of two tuples", function ()
+  local a = Vector:new(1, 2, 3)
+  local b = Vector:new(2, 3, 4)
+
+  assert.are.equal(20, a:dot(b))
+end)
+
+describe("The cross product of two vectors", function ()
+  local a = Vector:new(1, 2, 3)
+  local b = Vector:new(2, 3, 4)
+
+  assert.are.equals(Vector:new(-1, 2, -1), a:cross(b))
+  assert.are.equals(Vector:new(1, -2, 1), b:cross(a))
 end)
